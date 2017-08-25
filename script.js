153022e9d655ea2608685e9de12e53cd0bce88db
Avi Charlop <avicharlop@gmail.com>
2017-08-25 11:32:09 -0400

    initial script add

---
+++ script.js
@@ -0,0 +1,7 @@
+// get checkbox
+var checkbox = document.getElementById("id_close_anchor_branch")
+
+// if checkbox exists and it is not checked, check it
+if (checkbox && !checkbox.checked) {
+    checkbox.checked = true
+}
